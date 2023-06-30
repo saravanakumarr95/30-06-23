9cdf9697f7a018f1a5714140e65fa105e0cffe9c
saravanakumarr95 <saravanakumarr2703@gmail.com>
2023-06-30 12:55:18 +0530

    Today Pogramme

---
+++ Day4in3.java
@@ -0,0 +1,16 @@
+package org.programme;
+
+import java.util.Scanner;
+
+public class Day4in3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter the value");
+	int	size=scanner.nextInt();
+	int a[]=new int[size];
+	for(int i=0;i<=size;i=i+2) {
+		System.out.println(i);
+	}
+	}
+
+}
